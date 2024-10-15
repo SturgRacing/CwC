91eebbc18a44b244241dd601456f9a40274f32ac
SturgRacing <kadynsturgeon8806@gmail.com>
2024-10-15 10:52:21 -0400

    Jump and Death Animation

--- Unit 3/Assets/Scripts/PlayerController.cs
+++ Unit 3/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10.0f;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -14,7 +15,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= gravityModifier;
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier; 
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -38,6 +41,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("You Lose SUCKER");
+            playerAnim.SetBool("Death_b" , true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
